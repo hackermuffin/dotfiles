32e0eb689c513178e643da48e376dbae17734e53
Peter Oslington <peter.oslington@anu.edu.au>
2024-04-09 23:08:32 +1000

    Added telescope shortcuts

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,3 +1,6 @@
+-- set leader key
+vim.cmd('let mapleader=","')
+
 -- start a horizontal split terminal
 vim.api.nvim_create_user_command("Hterm", function()
 	vim.cmd([[
@@ -16,6 +19,13 @@ vim.api.nvim_create_user_command("Vterm", function()
     :startinsert
   ]])
 end, {})
+
+-- add telescope shortcuts
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- start NERDTree on startup
 local group = vim.api.nvim_create_augroup("NERDTreeAutostart", { clear = true })
