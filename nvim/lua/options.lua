3ab86366a6d3a5545a204854c35c76159e4e2b2d
Peter Oslington <peter.oslington@anu.edu.au>
2024-04-09 23:15:34 +1000

    Moved NERDTree config into plugin file

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -26,28 +26,3 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
--- start NERDTree on startup
-local group = vim.api.nvim_create_augroup("NERDTreeAutostart", { clear = true })
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		-- local argc = vim.api.nvim_eval("argc()")
-		local argc = vim.fn.argc()
-		local argv = vim.fn.argv()
-		if argc == 0 then
-			-- if no args, start NERDTree focused
-			vim.cmd("NERDTree")
-		elseif argc == 1 and vim.fn.isdirectory(argv[1]) == 1 then
-			-- if directory, cd to directory and start NERDTree focuses with empty buffer
-			vim.cmd("execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0]")
-		else
-			vim.cmd("NERDTree | wincmd p")
-		end
-	end,
-	group = group,
-})
-
--- close NERDTree if last open buffer
-vim.cmd([[
-autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
-]])
--- nvim/lua/plugins/nerdtree.lua
+++ nvim/lua/plugins/nerdtree.lua
@@ -1,3 +1,30 @@
 return {
 	"preservim/nerdtree",
+	config = function()
+		-- start NERDTree on startup
+		local group = vim.api.nvim_create_augroup("NERDTreeAutostart", { clear = true })
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				-- local argc = vim.api.nvim_eval("argc()")
+				local argc = vim.fn.argc()
+				local argv = vim.fn.argv()
+				if argc == 0 then
+					-- if no args, start NERDTree focused
+					vim.cmd("NERDTree")
+				elseif argc == 1 and vim.fn.isdirectory(argv[1]) == 1 then
+					-- if directory, cd to directory and start NERDTree focuses with empty buffer
+					vim.cmd("execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0]")
+				else
+					vim.cmd("NERDTree | wincmd p")
+				end
+			end,
+			group = group,
+		})
+
+		-- close NERDTree if last open buffer
+		vim.api.nvim_create_augroup("NERDTreeAutoquit", { clear = true })
+		vim.api.nvim_create_autocmd("BufEnter", {
+			command = "if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif",
+		})
+	end,
 }
