364ced431b85569ca82c4d1adf458b3569230640
Peter Oslington <peter.oslington@anu.edu.au>
2024-04-16 12:04:14 +1000

    Added basic jupyter notebooks support

---
+++ nvim/lua/plugins/magma.lua
@@ -0,0 +1,40 @@
+return {
+	"meatballs/magma-nvim",
+	config = function()
+		-- bootstrap python venv if not present
+		local venv_dir = vim.fn.stdpath("data") .. "/venv"
+		local python_bin = venv_dir .. "/bin/python"
+		if vim.fn.isdirectory(venv_dir) == 0 then
+			vim.fn.system({
+				"python3",
+				"-m",
+				"venv",
+				venv_dir,
+				"--system-site-packages",
+			})
+			vim.fn.system({
+				python_bin,
+				"-m",
+				"pip",
+				"install",
+				"pynvim",
+				"jupyter-client",
+				"ueberzug",
+				"pillow",
+				"cairosvg",
+				"pnglatex",
+				"plotly",
+				"kaleido",
+			})
+		end
+
+		-- set python venv
+		vim.cmd("let g:python3_host_prog = '" .. python_bin .. "'")
+
+		-- set up autorun of remote plugin loading
+		vim.api.nvim_create_augroup("RemotePluginLoad", { clear = true })
+		vim.api.nvim_create_autocmd("VimEnter", {
+			command = "UpdateRemotePlugins",
+		})
+	end,
+}
---
+++ nvim/lua/plugins/notebook-nvim.lua
@@ -0,0 +1,61 @@
+return {
+	"meatballs/notebook.nvim",
+	dependencies = {
+		"meatballs/magma-nvim",
+	},
+	opts = function()
+		local function dump(o)
+			if type(o) == "table" then
+				local s = "{ "
+				for k, v in pairs(o) do
+					if type(k) ~= "number" then
+						k = '"' .. k .. '"'
+					end
+					s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+				end
+				return s .. "} "
+			else
+				return tostring(o)
+			end
+		end
+		-- helper functions to set ipynb cells as magma cells
+		local api = require("notebook.api")
+		local settings = require("notebook.settings")
+		function _G.define_cell(extmark)
+			if extmark == nil then
+				extmark, _ = api.current_extmark()
+			end
+			local start_line = extmark[1] + 1
+			local end_line = extmark[3].end_row
+			print(start_line, end_line)
+			pcall(function()
+				vim.fn.MagmaDefineCell(start_line, end_line)
+			end)
+		end
+		function _G.define_all_cells()
+			local buffer = vim.api.nvim_get_current_buf()
+			local extmarks = settings.extmarks[buffer]
+			if extmarks ~= nil then
+				for id, cell in pairs(extmarks) do
+					local extmark =
+						vim.api.nvim_buf_get_extmark_by_id(0, settings.plugin_namespace, id, { details = true })
+					if cell.cell_type == "code" then
+						define_cell(extmark)
+					end
+				end
+			end
+		end
+
+		vim.api.nvim_create_augroup("IpynbMagmaInit", { clear = true })
+		vim.api.nvim_create_autocmd("BufRead", {
+			pattern = "*.ipynb",
+			command = "MagmaInit",
+			--group = "IpynbMagmaInit",
+		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = { "MagmaInitPost" },
+			callback = _G.define_all_cells,
+			--group = "IpynbMagmaInit",
+		})
+	end,
+}
