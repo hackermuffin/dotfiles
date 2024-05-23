36f1f245d8c52ecaf059ca3144a311bca7e14318
Peter Oslington <peter.oslington@anu.edu.au>
2024-05-23 13:03:47 +1000

    Added convenience config to latex setup

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,5 +4,19 @@ return {
 		vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
 		vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
 		vim.g.vimtex_view_method = "zathura"
+
+		local group = vim.api.nvim_create_augroup("texsetup", { clear = true })
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = "*.tex",
+			group = group,
+			callback = function()
+				-- Spell check latex documents
+				vim.opt.spelllang = "en_au"
+				vim.opt.spell = true
+				-- enable wordwrap
+				vim.cmd(":set wrap")
+				vim.cmd(":set linebreak")
+			end,
+		})
 	end,
 }
