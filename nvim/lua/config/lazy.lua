62c99ba2a5b151d49b056a4671e4f38a2837ca2c
Peter Oslington <peter.oslington@anu.edu.au>
2024-10-31 20:57:27 +1100

    Added rust using default lazy settings

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.haskell" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.tex" },
 		{ import = "plugins" },
 	},
